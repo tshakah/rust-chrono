7a0c2ad686189a5b1bb8f9ec9874e29137f334ea
Kang Seonghoon <public+git@mearie.org>
2015-02-27 13:09:10 +0900

    updated docs.

---
+++ implementors/core/num/trait.FromPrimitive.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['chrono'] = ["<a class='stability Unstable' title='Unstable: trait is likely to be removed'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/num/trait.FromPrimitive.html' title='core::num::FromPrimitive'>FromPrimitive</a> for <a class='enum' href='chrono/enum.Weekday.html' title='chrono::Weekday'>Weekday</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/core/ops/trait.Neg.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['chrono'] = ["<a class='stability Unstable' title='Unstable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/ops/trait.Neg.html' title='core::ops::Neg'>Neg</a> for <a class='struct' href='chrono/duration/struct.Duration.html' title='chrono::duration::Duration'>Duration</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
